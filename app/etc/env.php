284231352051fe3e9a1c57819ec6e3aeb2985ccd
Tobias Schifftner <ts@ambimax.de>
2016-07-02 13:01:33 +0200

    Added missing app/etc

---
+++ app/etc/env.php
@@ -0,0 +1,63 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_19288e',
+  ),
+  'crypt' => 
+  array (
+    'key' => '122fca2cfd180b0756422f98099fd55b',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'dev2',
+        'username' => 'root',
+        'password' => 'root',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sat, 02 Jul 2016 09:30:51 +0000',
+  ),
+);
